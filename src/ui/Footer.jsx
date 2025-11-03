70ed6d76948cf13fb80e47871f17def0fafdaf56
Dani <162477407+Dani008pro@users.noreply.github.com>
2025-11-04 00:55:38 +0330

    add copyright text

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -9,47 +9,51 @@ const Footer = () => {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.1 }}
         transition={{ duration: 0.8, ease: 'easeOut' }}
-        className="flex flex-col items-center justify-around sm:flex-row lg:flex-row"
       >
-        <Link to="/" className="flex flex-col items-center justify-center">
-          <img
-            src="/icons/footer.png"
-            className="w-[140px] lg:w-[180px]"
-            alt=""
-          />
-          <h2 className="text-center font-titr text-xl text-white lg:text-4xl">
-            CofeCo.
-          </h2>
-        </Link>
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="mb-10 font-titr text-xl text-white lg:text-4xl">
-            GET IN TOUCH
-          </h2>
-          <h2 className="font-titr text-2xl text-white">+12 345 67890</h2>
-          <a
-            className="mt-2 font-titr text-lg text-white lg:text-2xl"
-            href="mailto:info@example.com"
-          >
-            info@example.com
-          </a>
+        <div className="flex flex-col items-center justify-around sm:flex-row lg:flex-row">
+          <Link to="/" className="flex flex-col items-center justify-center">
+            <img
+              src="/icons/footer.png"
+              className="w-[140px] lg:w-[180px]"
+              alt=""
+            />
+            <h2 className="text-center font-titr text-xl text-white lg:text-4xl">
+              CofeCo.
+            </h2>
+          </Link>
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="mb-10 font-titr text-xl text-white lg:text-4xl">
+              GET IN TOUCH
+            </h2>
+            <h2 className="font-titr text-2xl text-white">+12 345 67890</h2>
+            <a
+              className="mt-2 font-titr text-lg text-white lg:text-2xl"
+              href="mailto:info@example.com"
+            >
+              info@example.com
+            </a>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="mb-10 font-titr text-xl text-white lg:text-4xl">
+              OPEN HOURS
+            </h2>
+            <h2 className="mb-3 font-titr text-lg text-white lg:text-2xl">
+              Monday-Friday
+            </h2>
+            <h2 className="mb-8 font-titr text-lg text-white lg:text-2xl">
+              8.00 AM - 8.00 PM
+            </h2>
+            <h2 className="mb-3 font-titr text-2xl text-white">
+              Saturday - Sunday
+            </h2>
+            <h2 className="font-titr text-lg text-white lg:text-2xl">
+              2.00 PM - 8.00 PM
+            </h2>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="mb-10 font-titr text-xl text-white lg:text-4xl">
-            OPEN HOURS
-          </h2>
-          <h2 className="mb-3 font-titr text-lg text-white lg:text-2xl">
-            Monday-Friday
-          </h2>
-          <h2 className="mb-8 font-titr text-lg text-white lg:text-2xl">
-            8.00 AM - 8.00 PM
-          </h2>
-          <h2 className="mb-3 font-titr text-2xl text-white">
-            Saturday - Sunday
-          </h2>
-          <h2 className="font-titr text-lg text-white lg:text-2xl">
-            2.00 PM - 8.00 PM
-          </h2>
-        </div>
+        <p className="mt-20 text-center text-white">
+          &copy; {new Date().getFullYear()} CafeCo. All rights reserved.
+        </p>
       </motion.div>
     </div>
   )
