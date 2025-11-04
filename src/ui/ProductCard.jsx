8d6ea5020445924b4094c1c1a88527d2aa53415d
Dani <162477407+Dani008pro@users.noreply.github.com>
2025-11-04 21:43:35 +0330

    fix the more link

--- src/ui/ProductCard.jsx
+++ src/ui/ProductCard.jsx
@@ -6,6 +6,12 @@ import { formatCurrency } from '../utils/helper'
 import { motion } from 'framer-motion'
 import Button from './Button'
 const ProductCard = ({ item, scale, blur }) => {
+  const words = item.description.split(' ')
+  const showMore = words.length > 5
+  const displayedText = showMore
+    ? words.slice(0, 5).join(' ') + '...'
+    : item.description
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
@@ -14,7 +20,13 @@ const ProductCard = ({ item, scale, blur }) => {
       transition={{ duration: 0.2, ease: 'backInOut' }}
       className={`relative flex h-[150px] transition-all duration-300 ease-in-out sm:h-[450px] sm:w-[300px] md:h-[460px] md:w-[335px] lg:h-[480px] lg:w-[350px] ${scale && 'scale-60'} ${blur && 'blur-md'} w-full flex-col items-center justify-center gap-2 rounded-2xl bg-box-300 px-5 md:gap-5 lg:gap-5`}
     >
-      <img src={`${item.image}`} alt="" className="hidden md:block lg:block" />
+      <Link to={`/product/${item.id}`}>
+        <img
+          src={`${item.image}`}
+          alt=""
+          className="hidden cursor-pointer md:block lg:block"
+        />
+      </Link>
       <h3 className="font-titr text-[16px] text-dark-500 sm:text-[20px] lg:text-[24px]">
         {item.title}
       </h3>
@@ -27,10 +39,12 @@ const ProductCard = ({ item, scale, blur }) => {
         </span>
       </div>
       <p className="hidden text-center font-text text-[16px] text-dark-500 sm:block sm:text-[18px] lg:block lg:text-[15px]">
-        {item.description}
-        <Link to="/product/:id" className="text-sky-700">
-          more
-        </Link>
+        {displayedText}
+        {showMore && (
+          <Link to={`/product/${item.id}`} className="text-sky-700">
+            more
+          </Link>
+        )}
       </p>
       <div className="flex w-full items-center justify-evenly">
         <span className="flex items-center justify-center gap-1 font-titr text-sm font-bold text-dark-500 sm:gap-2 lg:gap-3">
