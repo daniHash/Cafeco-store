989afad2009a0f168efae464bcc9a472bcbfcff2
Dani <162477407+Dani008pro@users.noreply.github.com>
2025-11-17 23:00:16 +0330

    add checkout design

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import Button from '../ui/Button'
 const Cart = () => {
   const navigate = useNavigate()
   return (
-    <div className="flex h-screen flex-col px-5 pt-5 sm:px-10 md:px-20 md:pt-16 lg:px-[140px] lg:pt-16">
+    <div className="flex flex-col px-5 pt-5 sm:px-10 md:px-20 md:pt-16 lg:px-[140px] lg:pt-16">
       <div className="flex w-full items-center justify-between">
         <h1 className="text font-titr text-xl text-dark-500 lg:text-4xl">
           My Cart
@@ -21,6 +21,40 @@ const Cart = () => {
             Back
           </span>
         </Button>
+      </div>
+      <div className="mt-20 h-64 w-full rounded-xl bg-[#D5B690]/65 shadow-form shadow-black/20"></div>
+      <div className="mt-20 mb-20 h-auto w-full rounded-xl bg-[#D5B690]/65 shadow-form shadow-black/20">
+        <div className="mt-20 flex w-full items-center justify-center gap-72">
+          <div className="">
+            <h2 className="text-center font-titr text-4xl text-dark-500">
+              Shipping
+            </h2>
+            <h3 className="mt-10 text-center font-titr text-lg text-dark-500 lg:text-[28px]">
+              Free
+            </h3>
+          </div>
+          <div className="">
+            <h2 className="text-center font-titr text-4xl text-dark-500">
+              Sub total
+            </h2>
+            <h3 className="mt-10 text-center font-titr text-lg text-dark-500 lg:text-[28px]">
+              300$
+            </h3>
+          </div>
+        </div>
+        <div className="">
+          <h2 className="mt-16 w-full text-center font-titr text-4xl text-destructive-300">
+            Sub total
+          </h2>
+          <h3 className="mt-10 text-center font-titr text-lg text-destructive-300 lg:text-[28px]">
+            300$
+          </h3>
+        </div>
+        <div className="mt-20 flex w-full justify-center">
+          <Button classType="primary" px={20}>
+            Chekout <span className="ml-20">300$</span>
+          </Button>
+        </div>
       </div>
     </div>
   )
