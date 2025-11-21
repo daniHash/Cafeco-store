e1b1f0ffbb7ef4555e12b7969e4dc7e6e7531600
Dani <162477407+Dani008pro@users.noreply.github.com>
2025-11-21 21:44:51 +0330

    fix the motion

--- src/ui/CartItems.jsx
+++ src/ui/CartItems.jsx
@@ -2,7 +2,7 @@ import CartItem from './CartItem'
 
 const CartItems = () => {
   return (
-    <div className="mt-20 h-auto w-full overflow-auto overflow-hidden rounded-xl bg-[#D5B690]/65 shadow-form shadow-black/20">
+    <div className="mt-20 h-auto w-full overflow-hidden rounded-xl bg-[#D5B690]/65 shadow-form shadow-black/20">
       <ul className="flex h-full w-full flex-col items-center justify-center divide-y-2 divide-dark-500">
         <CartItem />
         <CartItem />
