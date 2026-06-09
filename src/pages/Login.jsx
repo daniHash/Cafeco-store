0edbf44901f8509ae39ae3a9f01c40fe9fddbabf
danihash <danihashdev@gmail.com>
2026-06-09 21:41:47 +0330

    Initial commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,8 @@ import LoginForm from '../features/auth/LoginForm'
 
 const Login = () => {
   return (
-    <div>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <LoginForm />
-      </div>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <LoginForm />
     </div>
   )
 }
